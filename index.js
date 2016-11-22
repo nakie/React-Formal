4539042330540a590deccbc5b348c7d10175d6c9
Nathan Crews <nbcrews@gmail.com>
2016-11-22 14:51:27 -0600

    relocate index.js
    Moving indexjs away from src and into root dir.

---
+++ index.js
@@ -0,0 +1,16 @@
+import Input          from './src/components/Input';
+import Label          from './src/components/Label';
+import Option         from './src/components/Option';
+import Checkbox       from './src/components/Checkbox';
+import Radio          from './src/components/Radio';
+import OptionGroup    from './src/components/Option';
+
+
+module.exports = {
+    Checkbox,
+    Input,
+    Label,
+    Option,
+    Radio,
+    OptionGroup
+}
